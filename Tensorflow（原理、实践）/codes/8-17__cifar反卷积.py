269a3f46058cc16f4c43ba5f2167be6d1d74d81e
lllhhh <LLLLHHHH@126.com>
2018-10-30 16:11:27 +0800

    complete the 21th lesson.

--- Tensorflow（原理、实践）/codes/8-17__cifar反卷积.py
+++ Tensorflow（原理、实践）/codes/8-17__cifar反卷积.py
@@ -48,25 +48,27 @@ images_test, labels_test = cifar10_input.inputs(eval_data = True, data_dir = dat
 print("begin data")
 
 
-
 def weight_variable(shape):
-  initial = tf.truncated_normal(shape, stddev=0.01)
-  return tf.Variable(initial)
+    initial = tf.truncated_normal(shape, stddev=0.01)
+    return tf.Variable(initial)
+
 
 def bias_variable(shape):
-  initial = tf.constant(0.01, shape=shape)
-  return tf.Variable(initial)
-  
+    initial = tf.constant(0.01, shape=shape)
+    return tf.Variable(initial)
+
+
 def conv2d(x, W):
-  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
 
 def max_pool_2x2(x):
-  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                        strides=[1, 2, 2, 1], padding='SAME')  
-                        
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                        strides=[1, 2, 2, 1], padding='SAME')
+
+
 def avg_pool_6x6(x):
-  return tf.nn.avg_pool(x, ksize=[1, 6, 6, 1],
-                        strides=[1, 6, 6, 1], padding='SAME')
+    return tf.nn.avg_pool(x, ksize=[1, 6, 6, 1], strides=[1, 6, 6, 1], padding='SAME')
 
 # tf Graph Input
 x = tf.placeholder(tf.float32, [batch_size, 24,24,3]) # cifar data image of shape 24*24*3
@@ -131,19 +133,19 @@ summary_writer = tf.summary.FileWriter('./log/', sess.graph)
 
 tf.train.start_queue_runners(sess=sess)
 
-for i in range(15000):#20000
-  image_batch, label_batch = sess.run([images_train, labels_train])
-  label_b = np.eye(10,dtype=float)[label_batch] #one hot
+for i in range(15000): # 20000
+    image_batch, label_batch = sess.run([images_train, labels_train])
+    label_b = np.eye(10,dtype=float)[label_batch] #one hot
   
-  train_step.run(feed_dict={x:image_batch, y: label_b},session=sess)
-  #_, decoding_summary = sess.run([train_step, decoding_summary_op],feed_dict={x:image_batch, y: label_b})
+    train_step.run(feed_dict={x:image_batch, y: label_b},session=sess)
+    #_, decoding_summary = sess.run([train_step, decoding_summary_op],feed_dict={x:image_batch, y: label_b})
   
-  if i%200 == 0:
-    train_accuracy = accuracy.eval(feed_dict={
+    if i % 200 == 0:
+        train_accuracy = accuracy.eval(feed_dict={
         x:image_batch, y: label_b},session=sess)
-    print( "step %d, training accuracy %g"%(i, train_accuracy))
-    print("cross_entropy",cross_entropy.eval(feed_dict={x:image_batch, y: label_b},session=sess))
-    #summary_writer.add_summary(decoding_summary)
+        print("step %d, training accuracy %g" % (i, train_accuracy))
+        print("cross_entropy",cross_entropy.eval(feed_dict={x:image_batch, y: label_b},session=sess))
+        # summary_writer.add_summary(decoding_summary)
 
 
 image_batch, label_batch = sess.run([images_test, labels_test])
