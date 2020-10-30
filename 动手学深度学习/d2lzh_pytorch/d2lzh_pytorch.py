b7c6b81fcc91b8c46a57425671486da74749b35c
lllhhh <LLLLHHHH@126.com>
2020-10-30 17:49:05 +0800

    多输入通道和多输出通道
    添加多输入通道和多输出通道。

---
+++ 动手学深度学习/d2lzh_pytorch/d2lzh_pytorch.py
@@ -0,0 +1,10 @@
+import torch
+from torch import nn
+
+def corr2d(X, K):
+    h, w = K.shape
+    Y = torch.zeros((X.shape[0] - h + 1, X.shape[1] - w + 1))
+    for i in range(Y.shape[0]):
+        for j in range(Y.shape[1]):
+            Y[i, j] = (X[i: i + h, j: j + w] * K ).sum()
+    return Y
