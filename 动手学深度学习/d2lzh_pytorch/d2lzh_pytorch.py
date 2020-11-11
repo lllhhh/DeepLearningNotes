ef90b61cb7f3acbc4fe3d48d183b213d12b66252
lllhhh <LLLLHHHH@126.com>
2020-11-11 17:39:52 +0800

    LeNet
    添加LeNet。

--- 动手学深度学习/d2lzh_pytorch/d2lzh_pytorch.py
+++ 动手学深度学习/d2lzh_pytorch/d2lzh_pytorch.py
@@ -1,5 +1,7 @@
 import torch
 from torch import nn
+import torchvision
+import torchvision.transforms as transforms
 
 def corr2d(X, K):
     h, w = K.shape
@@ -8,3 +10,14 @@ def corr2d(X, K):
         for j in range(Y.shape[1]):
             Y[i, j] = (X[i: i + h, j: j + w] * K ).sum()
     return Y
+
+def load_data_fashion_mnist(batch_size):
+    mnist_train = torchvision.datasets.FashionMNIST(root='~/Datasets/FashionMNIST', train=True, download=True, transform=transforms.ToTensor())
+    mnist_test = torchvision.datasets.FashionMNIST(root='~/Datasets/FashionMNIST', train=False, download=True, transform=transforms.ToTensor())
+    if sys.platform.startswith('win'):
+        num_workers = 0  # 0表示不用额外的进程来加速读取数据
+    else:
+        num_workers = 4
+    train_iter = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    test_iter = torch.utils.data.DataLoader(mnist_test, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    return (train_iter, test_iter)
