e13ba107e54afcaa33a0fe43e581846c8d85784f
lllhhh <llllhhhh@126.com>
2020-12-13 16:09:35 +0800

    语言模型
    添加语言模型、采样。

--- 动手学深度学习/d2lzh_pytorch/d2lzh_pytorch.py
+++ 动手学深度学习/d2lzh_pytorch/d2lzh_pytorch.py
@@ -5,6 +5,7 @@ import torchvision.transforms as transforms
 import sys
 import time
 import torch.nn.functional as F
+import random
 
 class FlattenLayer(nn.Module):
     def __init__(self):
@@ -99,3 +100,41 @@ def train_ch5(net, train_iter, test_iter, batch_size, optimizer, device, num_epo
         test_acc = evaluate_accuracy(test_iter, net)
         print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f, time %.1f sec'
               % (epoch + 1, train_l_sum / batch_count, train_acc_sum / n, test_acc, time.time() - start))
+
+# 相邻采样
+def data_iter_consecutive(corpus_indices, batch_size, num_steps, device=None):
+    if device is None:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    corpus_indices = torch.tensor(corpus_indices, dtype=torch.float32, device=device)
+    data_len = len(corpus_indices)
+    batch_len = data_len // batch_size
+    indices = corpus_indices[0: batch_size * batch_len].view(batch_size, batch_len)
+    epoch_size = (batch_len - 1) // num_steps
+
+    for i in range(epoch_size):
+        i = i * num_steps
+        X = indices[:, i: i + num_steps ]
+        Y = indices[:, i + 1: i + num_steps + 1]
+        yield X, Y
+
+# 随机采样
+def data_iter_random(corpus_indices, batch_size, num_steps, device=None):
+    # 减一是因为输出的索引x是相应输入的索引y加1
+    num_examples = (len(corpus_indices) - 1 ) // num_steps
+    epoch_size = num_examples // batch_size
+    exapmple_indices = list(range(num_examples))
+    random.shuffle(exapmple_indices)
+
+    #返回从pos开始的长为num_steps的序列
+    def _data(pos):
+        return corpus_indices[pos: pos + num_steps]
+    
+    if device is None:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    for i in range(epoch_size):
+        i = i * batch_size
+        batch_indices = exapmple_indices[i: i + batch_size]
+        X = [ _data(j * num_steps) for j in batch_indices ]
+        Y = [ _data(j * num_steps + 1)  for j in batch_indices]
+        yield torch.tensor(X, dtype=torch.float32, device=device), torch.tensor(Y, dtype=torch.float32, device=device)
